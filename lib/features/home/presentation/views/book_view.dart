fe6d305b73f20c29f617bb28c80a8737677e9fb1
Amira Tarek <amirat737@gmail.com>
2024-09-15 13:26:06 +0300

    fix layout responsiveness of bookview

--- lib/features/home/presentation/views/book_view.dart
+++ lib/features/home/presentation/views/book_view.dart
@@ -1,12 +1,31 @@
+import 'package:bookly/core/utils/color_palette.dart';
 import 'package:bookly/features/home/data/models/book_model.dart';
 import 'package:bookly/features/home/presentation/views/widgets/book_bottom_section_widget.dart';
 import 'package:bookly/features/home/presentation/views/widgets/book_overlay_widget.dart';
 import 'package:bookly/features/home/presentation/views/widgets/book_top_section_widget.dart';
 import 'package:flutter/material.dart';
 
-class BookView extends StatelessWidget {
+class BookView extends StatefulWidget {
   final BookModel bookModel;
   const BookView({super.key, required this.bookModel});
+
+  @override
+  State<BookView> createState() => _BookViewState();
+}
+
+class _BookViewState extends State<BookView> {
+  final GlobalKey _globalKey = GlobalKey();
+  double sectionHeight = 0;
+
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback(
+      (_) {
+        _getSectionHeight();
+      },
+    );
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -17,17 +36,30 @@ class BookView extends StatelessWidget {
           children: [
             Column(
               children: [
-                BookTopSectionWidget(bookModel: bookModel),
-                BookBottomSectionWidget(bookModel: bookModel),
+                Container(
+                  key: _globalKey,
+                  color: ColorPalette.kPrimaryColor,
+                  child: BookTopSectionWidget(bookModel: widget.bookModel),
+                ),
+                BookBottomSectionWidget(bookModel: widget.bookModel),
               ],
             ),
-            Positioned(
-              top: 580 - 25,
-              child: BookOverlayWidget(bookModel: bookModel),
-            )
+            if (sectionHeight > 0)
+              Positioned(
+                top: sectionHeight - 25,
+                child: BookOverlayWidget(bookModel: widget.bookModel),
+              )
           ],
         ),
       ),
     );
+  }
+
+  void _getSectionHeight() {
+    final renderBox =
+        _globalKey.currentContext?.findRenderObject() as RenderBox;
+    setState(() {
+      sectionHeight = renderBox.size.height;
+    });
   }
 }
--- lib/features/home/presentation/views/widgets/book_top_section_widget.dart
+++ lib/features/home/presentation/views/widgets/book_top_section_widget.dart
@@ -1,4 +1,3 @@
-import 'package:bookly/core/utils/color_palette.dart';
 import 'package:bookly/features/home/data/models/book_model.dart';
 import 'package:bookly/features/home/presentation/views/widgets/book_appbar_widget.dart';
 import 'package:bookly/features/home/presentation/views/widgets/book_details_widget.dart';
@@ -6,24 +5,19 @@ import 'package:flutter/material.dart';
 
 class BookTopSectionWidget extends StatelessWidget {
   final BookModel bookModel;
-
   const BookTopSectionWidget({super.key, required this.bookModel});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 580,
-      color: ColorPalette.kPrimaryColor,
-      child:  SafeArea(
-        child: Column(
-          children: [
-            const SizedBox(height: 15),
-            const BookAppbarWidget(),
-            const SizedBox(height: 30),
-            BookDetailsWidget(bookModel: bookModel),
-            const SizedBox(height: 50),
-          ],
-        ),
+    return SafeArea(
+      child: Column(
+        children: [
+          const SizedBox(height: 15),
+          const BookAppbarWidget(),
+          const SizedBox(height: 30),
+          BookDetailsWidget(bookModel: bookModel),
+          const SizedBox(height: 50),
+        ],
       ),
     );
   }
