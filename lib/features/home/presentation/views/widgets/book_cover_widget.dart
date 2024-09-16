d13b69859523a41443f8bb73101f738dbcc86e4a
Amira Tarek <amirat737@gmail.com>
2024-09-16 03:39:58 +0300

    Temporary fix Cashed url Exception

--- lib/features/home/presentation/views/widgets/book_cover_widget.dart
+++ lib/features/home/presentation/views/widgets/book_cover_widget.dart
@@ -1,11 +1,14 @@
+import 'package:bookly/core/utils/assets_data.dart';
+import 'package:bookly/core/utils/color_palette.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class BookCoverWidget extends StatelessWidget {
   final String imageUrl;
   final double radius;
   final double ratio;
-  
+
   const BookCoverWidget({
     super.key,
     required this.radius,
@@ -15,6 +18,16 @@ class BookCoverWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //Temporary fix Cashed url Exception
+    FlutterError.onError = (flutterErrorDetails) {
+      if (flutterErrorDetails.library == "image resource service" &&
+          flutterErrorDetails.exception
+              .toString()
+              .startsWith("HttpException: Invalid statusCode: 404, uri")) {
+        return;
+      }
+      return;
+    };
     return AspectRatio(
       aspectRatio: ratio,
       child: ClipRRect(
@@ -23,7 +36,16 @@ class BookCoverWidget extends StatelessWidget {
           imageUrl: imageUrl,
           fit: BoxFit.fill,
           errorWidget: (context, url, error) {
-            return const Icon(Icons.error);
+            return Image.asset(
+              AssetsData.imagePlaceholder,
+              fit: BoxFit.fill,
+            );
+          },
+          progressIndicatorBuilder: (context, url, progress) {
+            return const SpinKitCubeGrid(
+              color: ColorPalette.kPrimaryColor,
+              size: 20,
+            );
           },
         ),
       ),
