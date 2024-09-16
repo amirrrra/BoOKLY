d2acf4a851d7ef0c1f3eaa02a8c11beda2a9526c
Amira Tarek <amirat737@gmail.com>
2024-09-16 03:36:28 +0300

    build spinkit indicator

--- lib/core/widgets/loading_widget.dart
+++ lib/core/widgets/loading_widget.dart
@@ -1,15 +1,14 @@
 import 'package:bookly/core/utils/color_palette.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class LoadingWidget extends StatelessWidget {
   const LoadingWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: CircularProgressIndicator(
-        color: ColorPalette.kPrimaryColor,
-      ),
+    return const SpinKitSquareCircle(
+      color: ColorPalette.kPrimaryColor,
     );
   }
 }
