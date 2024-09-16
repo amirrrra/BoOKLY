71d67de46d74964354342ee16e589a6020fc7cc0
Amira Tarek <amirat737@gmail.com>
2024-09-16 03:39:00 +0300

    rebuild book button

--- lib/features/home/presentation/views/widgets/book_button_widget.dart
+++ lib/features/home/presentation/views/widgets/book_button_widget.dart
@@ -1,44 +1,49 @@
+import 'package:bookly/core/utils/assets_data.dart';
 import 'package:bookly/core/utils/color_palette.dart';
 import 'package:bookly/core/utils/styles.dart';
+import 'package:bookly/features/home/data/models/book_model.dart';
 import 'package:flutter/material.dart';
-class BookButtonWidget extends StatelessWidget {
-  final String text;
-  final double leftRadius;
-  final double rightRadius;
-  final void Function()? onPressed;
+import 'package:flutter_svg/svg.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-  const BookButtonWidget({
-    super.key,
-    required this.leftRadius,
-    required this.rightRadius,
-    required this.text,
-    required this.onPressed,
-  });
+class BookButtonWidget extends StatelessWidget {
+  final BookModel bookModel;
+
+  const BookButtonWidget({super.key, required this.bookModel});
 
   @override
   Widget build(BuildContext context) {
     return TextButton(
       style: TextButton.styleFrom(
-        fixedSize: const Size(150, 50),
+        fixedSize: const Size(250, 60),
         backgroundColor: ColorPalette.kBabyBlack,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.only(
-            topRight: Radius.circular(rightRadius),
-            bottomRight: Radius.circular(rightRadius),
-            topLeft: Radius.circular(leftRadius),
-            bottomLeft: Radius.circular(leftRadius),
-          ),
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(15)),
         ),
       ),
-      onPressed: onPressed,
-      child: Center(
-        child: Text(
-          text,
-          style: Styles.style17.copyWith(
-            color: ColorPalette.kWhite,
+      onPressed: () => launchLink(bookModel.volumeInfo?.previewLink ?? ''),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SvgPicture.asset(AssetsData.openbook),
+          const SizedBox(width: 10,),
+          Text(
+            'READ BOOK',
+            style: Styles.style17.copyWith(
+              color: ColorPalette.kWhite,
+            ),
           ),
-        ),
+        ],
       ),
     );
+  }
+
+  Future<void> launchLink(String link) async {
+    final url = Uri.parse(link);
+    if (await canLaunchUrl(url)) {
+      await launchUrl(url);
+    } else {
+      throw Exception('Could not launch $url');
+    }
   }
 }
